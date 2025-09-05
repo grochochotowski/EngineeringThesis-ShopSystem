27193b381e09a5b4c38ef3d303480a234be28ab4
grochochotowski <g.m.rochowski@gmail.com>
2025-09-05 19:51:19 +0200

    Create relation between warehouse and product

---
+++ src/backend/Backend.Api/Objects/Entities/Models/Relations/WarehouseProduct.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Api.Objects.Entities.Models.Relations
+{
+    public class WarehouseProduct
+    {
+        [Required] public int WarehouseId { get; set; }
+        public virtual Warehouse Warehouse { get; set; } = default!;
+
+        [Required] public int ProductId { get; set; }
+        public virtual Product Product { get; set; } = default!;
+
+        [Required] public int Quantity { get; set; }
+    }
+}
