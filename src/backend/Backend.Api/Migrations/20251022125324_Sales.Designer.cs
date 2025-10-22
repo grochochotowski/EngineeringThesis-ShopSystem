feb3b55ca58d4e910283f40d6b1cd5f0c5aec39d
grochochotowski <g.m.rochowski@gmail.com>
2025-10-22 23:08:46 +0200

    migrate db

--- src/backend/Backend.Api/Migrations/20251022125324_Sales.Designer.cs
+++ src/backend/Backend.Api/Migrations/20251022125324_Sales.Designer.cs
@@ -4,6 +4,7 @@ using Backend.Api.Objects.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251022125324_Sales")]
+    partial class Sales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
