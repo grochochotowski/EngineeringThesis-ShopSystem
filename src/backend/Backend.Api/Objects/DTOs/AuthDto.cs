fb6c6a97b793dcf8463f3ccf317c24ce20699fe9
grochochotowski <g.m.rochowski@gmail.com>
2025-11-02 17:19:24 +0100

    create auth dto and update user dto for credential use

---
+++ src/backend/Backend.Api/Objects/DTOs/AuthDto.cs
@@ -0,0 +1,54 @@
+﻿using Backend.Api.Objects.Entities.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Api.Objects.DTOs
+{
+    // --- LOGIN ---
+    public class LoginDto
+    {
+        [Required, MaxLength(64)]               public string Login                 { get; set; } = default!;
+        [Required, MaxLength(128)]              public string Password              { get; set; } = default!;
+    }
+
+    // --- REGISTER ---
+    public class RegisterUserDto
+    {
+        [Required, MaxLength(64)]               public string FirstName             { get; set; } = default!;
+        [Required, MaxLength(64)]               public string LastName              { get; set; } = default!;
+        [Required, MaxLength(64), EmailAddress] public string Email                 { get; set; } = default!;
+        [Required, MaxLength(32), Phone]        public string PhoneNumber           { get; set; } = default!;
+        [Required]                              public DateTime DateOfBirth         { get; set; }
+        [Required]                              public UserRole Role                { get; set; }
+        [Required, MaxLength(64)]               public string Login                 { get; set; } = default!;
+        [Required, MaxLength(128)]              public string Password              { get; set; } = default!;
+                                                public int? AddressId               { get; set; }
+                                                public CreateAddressDto? Address    { get; set; }
+    }
+
+    // --- AUTH RESPONSE ---
+    public class AuthUserDto
+    {
+                                                public int Id                       { get; set; }
+                                                public string FirstName             { get; set; } = default!;
+                                                public string LastName              { get; set; } = default!;
+                                                public string Email                 { get; set; } = default!;
+                                                public UserRole Role                { get; set; } = default!;
+                                                public string AccessToken           { get; set; } = default!;
+                                                public string RefreshToken          { get; set; } = default!;
+    }
+
+    // --- REFRESH TOKEN ---
+    public class RefreshTokenDto
+    {
+        [Required]                              public string AccessToken           { get; set; } = default!;
+        [Required]                              public string RefreshToken          { get; set; } = default!;
+    }
+
+    // --- CHANGE PASSWORD ---
+    public class ChangePasswordDto
+    {
+        [Required, MaxLength(128)]              public string CurrentPassword       { get; set; } = default!;
+        [Required, MaxLength(128)]              public string NewPassword           { get; set; } = default!;
+        [Required, MaxLength(128)]              public string ConfirmNewPassword    { get; set; } = default!;
+    }
+}
--- src/backend/Backend.Api/Objects/DTOs/UserDto.cs
+++ src/backend/Backend.Api/Objects/DTOs/UserDto.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Api.Objects.DTOs
 {
+    // --- GET USER INFO ---
     public class GetUserDto
     {
                                                 public int Id                       { get; set; }
@@ -12,24 +13,21 @@ namespace Backend.Api.Objects.DTOs
                                                 public string PhoneNumber           { get; set; } = default!;
                                                 public DateTime DateOfBirth         { get; set; }
                                                 public UserRole Role                { get; set; }
-                                                public string Login                 { get; set; } = default!;
                                                 public int AddressId                { get; set; }
+                                                public GetAddressDto? Address { get; set; }
     }
 
-    public class CreateUserDto
+    // --- GET USER LIST INFO ---
+    public class GetUserListItemDto
     {
-        [Required, MaxLength(64)]               public string FirstName             { get; set; } = default!;
-        [Required, MaxLength(64)]               public string LastName              { get; set; } = default!;
-        [Required, MaxLength(64), EmailAddress] public string Email                 { get; set; } = default!;
-        [Required, MaxLength(32), Phone]        public string PhoneNumber           { get; set; } = default!;
-        [Required]                              public DateTime DateOfBirth         { get; set; }
-        [Required]                              public UserRole Role                { get; set; }
-        [Required, MaxLength(64)]               public string Login                 { get; set; } = default!;
-        [Required, MaxLength(256)]              public string Password              { get; set; } = default!;
-                                                public int? AddressId               { get; set; }
-                                                public CreateAddressDto? Address    { get; set; }
+                                                public int Id { get; set; }
+                                                public string FirstName { get; set; } = default!;
+                                                public string LastName { get; set; } = default!;
+                                                public string Email { get; set; } = default!;
+                                                public UserRole Role { get; set; }
     }
 
+    // --- UPDATE USET INFO ---
     public class UpdateUserDto
     {
         [Required, MaxLength(64)]               public string FirstName             { get; set; } = default!;
@@ -40,11 +38,5 @@ namespace Backend.Api.Objects.DTOs
         [Required]                              public UserRole Role                { get; set; }
                                                 public int? AddressId               { get; set; }
                                                 public CreateAddressDto? Address    { get; set; }
-    }
-
-    public class UpdateUserPasswordOrLoginDto
-    {
-        [Required, MaxLength(64)]               public string Login                 { get; set; } = default!;
-        [Required, MaxLength(256)]              public string Password              { get; set; } = default!;
     }
 }
