f2bee2686f8ad635826ef246758a3bc63103c532
grochochotowski <g.m.rochowski@gmail.com>
2025-11-05 00:44:04 +0100

    migrate db

---
+++ src/backend/Backend.Api/Migrations/20251104234237_servicesUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class servicesUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "DeliveryCompanies",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "DeliveryCompanies");
+        }
+    }
+}
