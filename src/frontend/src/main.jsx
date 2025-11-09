f2ae09940f460ddce094562dcab325a8f622455c
grochochotowski <g.m.rochowski@gmail.com>
2025-11-09 21:53:38 +0100

    update routing after login

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -11,23 +11,31 @@ import './styles/style.css'
 // Lazy loading pages
 const Login = lazy(() => import('./pages/Login'))
 const NotFound = lazy(() => import('./pages/ErrorPages/NotFound.jsx'))
+const Dashboard = lazy(() => import("./pages/Dashboard.jsx"));
 
+// Protected route wrapper
 const PrivateRoute = ({ children }) => {
     const { state } = useContext(GlobalStateContext);
-    return state.isLoggedIn ? children : <Navigate to="/404" />;
+    return state.isLoggedIn ? children : <Navigate to="/login" replace/>;
 };
 
+// Router definition
 const router = createBrowserRouter([
     { path: '/', element: <Login />, errorElement: <NotFound /> },
+
+    { path: '/dashboard', element:  <PrivateRoute><Dashboard /></PrivateRoute>, errorElement: <NotFound /> }, // i added this line
 
     { path: '/404', element: <NotFound /> },
     //{ path: '/fallback', element: <Fallback /> },
 ]);
 
+// Root render
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GlobalStateProvider>
-            <RouterProvider router={router} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <RouterProvider router={router} />
+            </Suspense>
         </GlobalStateProvider>
     </React.StrictMode>
 )
