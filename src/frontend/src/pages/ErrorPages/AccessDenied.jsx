e8a850f333584f9c0e8ccaba6e5ff095f347adca
grochochotowski <g.m.rochowski@gmail.com>
2025-11-10 21:45:22 +0100

    fix error routing

--- src/frontend/src/pages/ErrorPages/AccessDenied.jsx
+++ src/frontend/src/pages/ErrorPages/AccessDenied.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "../../styles/ErrorStyles/error.css";
+import { useNavigate } from "react-router-dom";
+import "../../styles/error.css";
 
 const AccessDenied = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="error-container">
             <div className="error-content">
                 <h1 style={{ color: "var(--btn-danger-bg)" }}>403</h1>
                 <h2>Access Denied</h2>
                 <p>You don’t have permission to view this page.</p>
-                <button onClick={() => (window.location.href = "/dashboard")}>
+                <button onClick={() => navigate("/dashboard")}>
                     Back to Dashboard
                 </button>
             </div>
--- src/frontend/src/pages/ErrorPages/NotFound.jsx
+++ src/frontend/src/pages/ErrorPages/NotFound.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "../../styles/ErrorStyles/error.css";
+import { useNavigate } from "react-router-dom";
+import "../../styles/error.css";
 
 const NotFound = () => {
+    const navigate = useNavigate();
+    
     return (
         <div className="error-container">
             <div className="error-content">
                 <h1 style={{ color: "var(--accent-main)" }}>404</h1>
                 <h2>Page Not Found</h2>
                 <p>The page you are looking for does not exist.</p>
-                <button onClick={() => (window.location.href = "/dashboard")}>
+                <button onClick={() => navigate("/dashboard")}>
                     Back to Dashboard
                 </button>
             </div>
